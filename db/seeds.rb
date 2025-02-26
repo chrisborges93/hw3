1663e0b2da4bca87de05dff340bfe48ba534cb12
Christian Borges <christian.borges@kellogg.northwestern.edu>
2025-02-26 22:33:06 +0000

    Filled Entries db and connected to show pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,25 @@
 
 places = Place.create([{ name: "New Mexico" }, { name: "Charleston" }, { name: "Beijing" }, { name: "Amsterdam" }])
 
+places = Place.all.index_by(&:name) 
 
+Entry.create([
+    
+   { title: "Ate Tacos", description: "Had some amazing tacos.", occurred_on: "2022-01-01", place_id: places["New Mexico"].id },
+   { title: "Lisa and Jim's Wedding", description: "Beautiful wedding celebration!", occurred_on: "2022-01-01", place_id: places["New Mexico"].id },
+   { title: "Ate Tacos Again", description: "Couldn't resist more tacos!", occurred_on: "2022-01-01", place_id: places["New Mexico"].id },
+
+   { title: "Ate Tacos", description: "Had some amazing tacos.", occurred_on: "2022-01-01", place_id: places["Charleston"].id },
+   { title: "Lisa and Jim's Wedding", description: "Beautiful wedding celebration!", occurred_on: "2022-01-01", place_id: places["Charleston"].id },
+   { title: "Ate Tacos Again", description: "Couldn't resist more tacos!", occurred_on: "2022-01-01", place_id: places["Charleston"].id },
+
+   { title: "Ate Tacos", description: "Had some amazing tacos.", occurred_on: "2022-01-01", place_id: places["Beijing"].id },
+   { title: "Lisa and Jim's Wedding", description: "Beautiful wedding celebration!", occurred_on: "2022-01-01", place_id: places["Beijing"].id },
+   { title: "Ate Tacos Again", description: "Couldn't resist more tacos!", occurred_on: "2022-01-01", place_id: places["Beijing"].id },
+
+   { title: "Ate Tacos", description: "Had some amazing tacos.", occurred_on: "2022-01-01", place_id: places["Amsterdam"].id },
+   { title: "Lisa and Jim's Wedding", description: "Beautiful wedding celebration!", occurred_on: "2022-01-01", place_id: places["Amsterdam"].id },
+   { title: "Ate Tacos Again", description: "Couldn't resist more tacos!", occurred_on: "2022-01-01", place_id: places["Amsterdam"].id }
+  ])
 
 
